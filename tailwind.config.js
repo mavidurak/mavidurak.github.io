8f67f216158d24fffd55f745909e7346919ab5ad
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2021-03-18 22:21:03 +0300

    Basic structure has been created

---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/**/*.md',
+    '*.html',
+  ],
+  darkMode: false,
+  theme: {
+    container: {
+      padding: '2rem',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      purple: colors.purple,
+      mavidurak: {
+        100: '#a7deff',
+        200: '#74caff',
+        300: '#41b7ff',
+        400: '#0ea4ff',
+        500: '#0087da',
+        600: '#0068a7',
+        700: '#004874', // primary color
+        800: '#00385a',
+        900: '#001927'
+      }
+    },
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'rubik': ['Rubik'],
+      'poppins': ['Poppins', 'sans-serif'],
+      'cormorant': ['Cormorant Unicase'],
+      'ubuntu': ['Ubuntu']
+    },
+    extend: {}
+  },
+  variants: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
