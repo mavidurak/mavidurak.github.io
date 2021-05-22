8db0ac286f974bf90cce8ce945e859a0f52c910c
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2021-05-22 13:31:17 +0300

    Fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: [
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/**/*.md',
-    '*.html',
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/**/*.md",
+    "./pages/**/*.md",
+    "*.html",
   ],
   darkMode: false,
   theme: {
     container: {
-      padding: '2rem',
+      padding: "2rem",
     },
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      transparent: "transparent",
+      current: "currentColor",
       black: colors.black,
       white: colors.white,
       gray: colors.trueGray,
@@ -23,31 +24,30 @@ module.exports = {
       yellow: colors.amber,
       purple: colors.purple,
       mavidurak: {
-        100: '#a7deff',
-        200: '#74caff',
-        300: '#41b7ff',
-        400: '#0ea4ff',
-        500: '#0087da',
-        600: '#0068a7',
-        700: '#004874', // primary color
-        800: '#00385a',
-        900: '#001927'
-      }
+        100: "#a7deff",
+        200: "#74caff",
+        300: "#41b7ff",
+        400: "#0ea4ff",
+        500: "#0087da",
+        600: "#0068a7",
+        700: "#004874", // primary color
+        800: "#00385a",
+        900: "#001927",
+      },
     },
     fontFamily: {
-      'sans': ['ui-sans-serif', 'system-ui'],
-      'serif': ['ui-serif', 'Georgia'],
-      'mono': ['ui-monospace', 'SFMono-Regular'],
-      'rubik': ['Rubik'],
-      'poppins': ['Poppins', 'sans-serif'],
-      'cormorant': ['Cormorant Unicase'],
-      'ubuntu': ['Ubuntu']
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      rubik: ["Rubik"],
+      poppins: ["Poppins", "sans-serif"],
+      cormorant: ["Cormorant Unicase"],
+      ubuntu: ["Ubuntu"],
     },
-    extend: {}
+    extend: {},
   },
   variants: {
-    extend: {
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
